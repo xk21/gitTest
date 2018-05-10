b9c5535074371f444d4f5d7d3e70245147191800
xk21 <15815593025@163.com>
2018-05-10 10:18:01 +0800

    Update MainActivity.java

--- app/src/main/java/com/jj/tt/gittest/MainActivity.java
+++ app/src/main/java/com/jj/tt/gittest/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //添加dev,十大撒旦法
+        //添加dev,十大1122撒旦法
     }
 }
