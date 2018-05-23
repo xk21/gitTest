b4c4fc833d87a2316316acfee493c26c16cc3e5e
xk21 <15815593025@163.com>
2018-05-23 10:27:38 +0800

    Update MainActivity.java

--- app/src/main/java/com/jj/tt/gittest/MainActivity.java
+++ app/src/main/java/com/jj/tt/gittest/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //添加dev,十大撒旦大大的
+        //大是大非
     }
 }
